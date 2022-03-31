ef0acc69051d6a2b88f04a9b150de6db10abce82
Thomas Eidens <thomas.eidens@embo.org>
2022-03-31 16:10:47 +0200

    Project setup

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <h1>RenderRev</h1>
+  </header>
+
+  <main>
+    <ul>
+      <li>Review 1</li>
+      <li>Review 2</li>
+      <li>...</li>
+    </ul>
+  </main>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+}
+</style>
