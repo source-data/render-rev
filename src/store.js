fb4059df84aa9d16ffa83a90ddb3cbd112b9818b
Thomas Eidens <thomas.eidens@embo.org>
2022-10-05 10:01:56 +0200

    Made options parameter optional

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
 import { parse as parseDocmap } from './docmaps.js';
 
-export function getReviewProcess(doi, options) {
+export async function getReviewProcess(doi, options) {
   if (!doi) {
     return new Promise((_, reject) => reject());
   }
-  const { source } = options;
+  const { source } = options || { source: 'eeb-docmaps' };
   switch (source) {
     case 'eeb-docmaps':
       return fetch(`https://eeb.embo.org/api/v2/docmap/${doi}`)
