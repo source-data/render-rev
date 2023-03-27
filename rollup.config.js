ee80d66dbc5ae08cb19e6d8c8304965a6195ac10
Thomas Eidens <thomas.eidens@embo.org>
2023-03-27 16:36:38 +0200

    Formatting for rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,24 +3,21 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 
 export default {
-	input: 'index.js',
-    output: [
-        {
-            file: 'dist/render-rev.js',
-            format: 'iife'
-        },
-        {
-            file: 'dist/render-rev.min.js',
-            format: 'iife',
-            plugins: [terser()]
-        },
-        {
-            file: 'dist/render-rev.es.js',
-            format: 'es'
-        }
-    ],
-    plugins: [
-        json(),
-        nodeResolve()
-    ]
+  input: 'index.js',
+  output: [
+    {
+      file: 'dist/render-rev.js',
+      format: 'iife',
+    },
+    {
+      file: 'dist/render-rev.min.js',
+      format: 'iife',
+      plugins: [terser()],
+    },
+    {
+      file: 'dist/render-rev.es.js',
+      format: 'es',
+    },
+  ],
+  plugins: [json(), nodeResolve()],
 };
