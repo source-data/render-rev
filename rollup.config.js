091b6760369fcf0eee82b3d40fa7e2774aa7fae1
Thomas Eidens <thomas.eidens@embo.org>
2023-12-22 10:49:08 +0100

    Fixed issues in rollup and package config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
@@ -6,20 +7,27 @@ export default {
   input: 'index.js',
   output: [
     {
-      file: 'dist/render-rev.js',
+      file: 'dist/render-rev.iife.js',
       format: 'iife',
       name: 'renderRev',
+      sourcemap: true,
     },
     {
-      file: 'dist/render-rev.min.js',
+      file: 'dist/render-rev.iife.min.js',
       format: 'iife',
       name: 'renderRev',
       plugins: [terser()],
+      sourcemap: true,
     },
     {
-      file: 'dist/render-rev.es.js',
+      file: 'dist/render-rev.esm.js',
       format: 'es',
+      sourcemap: true,
     },
   ],
-  plugins: [json(), nodeResolve()],
+  plugins: [
+    json(),
+    nodeResolve({ browser: true }), // Assume resolution for browser environment
+    commonjs(), // Convert CommonJS modules to ES6
+  ],
 };
